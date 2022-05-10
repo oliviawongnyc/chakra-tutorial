f499c406b0770e4f690a1c3eb8219b80f402ba01
Olivia Wong <oliviawong@nypl.org>
2022-05-10 16:04:37 -0400

    Updates for study group part 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
-const IndexPage = () => <h1>Hello Next.js 👋</h1>
+import { Container, Flex } from "@chakra-ui/react";
+import Details from "../src/sections/details";
+import Cart from "../src/sections/cart";
 
-export default IndexPage
+const IndexPage = () => (
+  <Container maxW="container.xl" p={0}>
+    <Flex h="100vh" py={20}>
+      <Details />
+      <Cart />
+    </Flex>
+  </Container>
+);
+
+export default IndexPage;
---
+++ src/sections/cart.tsx
@@ -0,0 +1,11 @@
+import { Heading, VStack } from "@chakra-ui/react";
+
+const Cart = () => {
+  return (
+    <VStack w="full" h="full" p={10} bg="gray.50">
+      <Heading size="l">⬇️ Build this side with Chakra components ⬇️</Heading>
+    </VStack>
+  );
+};
+
+export default Cart;
